9741a74c482139a05b4f46842e5f20ff77333e2e
Ashlee <chica.del.muerto@gmail.com>
2014-03-11 21:23:59 -0400

    defaltcodec
    Was unable to compile from source due to defaukdcodec. Changed to
    defaultcodec

--- src/qt/locale/bitcoin_en.ts
+++ src/qt/locale/bitcoin_en.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="en">
-<defaukdcodec>UTF-8</defaukdcodec>
+<defaultcodec>UTF-8</defaultcodec>
 <context>
     <name>AboutDialog</name>
     <message>
